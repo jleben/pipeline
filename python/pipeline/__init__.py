2c078b48312f2779c025f3bc2f56be9a517ae3a8
Jakob Leben <jakob.leben@gmail.com>
2017-06-14 17:57:35 -0700

    Python: Load scripts as modules and add their path to sys.path

--- python/pipeline/__init__.py
+++ python/pipeline/__init__.py
@@ -2,6 +2,7 @@
 import json
 import runpy
 import os.path
+import sys
 
 class Pipeline:
 
@@ -16,19 +17,27 @@ class Pipeline:
 
     def relativePath(self, path):
         if self.path:
-            return self.path + "/" + path
+            return os.path.join(self.path, path)
         else:
             return path
 
     def load(self, path):
 
-        current_path = self.path
+        child_file_path = os.path.abspath(self.relativePath(path))
+        child_dir = os.path.dirname(child_file_path)
+        child_name = os.path.splitext(os.path.basename(child_file_path))[0]
 
-        child_file_path = self.relativePath(path)
+        old_path = self.path
+        old_sys_path = sys.path;
 
-        self.path = os.path.dirname(child_file_path)
+        self.path = child_dir
+        if sys.path[0] != child_dir:
+            sys.path.insert(0,child_dir)
 
-        runpy.run_path(child_file_path,  { "pipeline": self })
+        runpy.run_module(child_name,  { "pipeline": self }, run_name = "")
+
+        self.path = old_path;
+        sys.path = old_sys_path
 
     def save(self, path=None):
 

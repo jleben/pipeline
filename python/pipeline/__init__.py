6af7135e62f8e822a6c126e7515273602d9dcdd5
Jakob Leben <jakob.leben@gmail.com>
2018-09-24 11:21:34 -0700

    Python: Add methods Pipeline.root_dir and Pipeline.current_dir

--- python/pipeline/__init__.py
+++ python/pipeline/__init__.py
@@ -7,38 +7,42 @@ from pathlib import Path
 
 class Pipeline:
 
-    tasks = [];
-
-    def __init__(self, args=[]):
-        self.tasks = [];
-        self.path = None;
-        self.args = args;
+    def __init__(self, root_path, args=[]):
+        self._tasks = [];
+        self._root_path = Path(root_path);
+        self._path = self._root_path;
+        self._args = args;
 
     def add(self, task):
-        self.tasks.append(task);
+        self._tasks.append(task);
 
     def relativePath(self, path):
-        if self.path is None:
-            return Path(path)
-        else:
-            return self.path / path
+        return self._path / path
+
+    def root_dir(self):
+        return self._root_path
+
+    def current_dir(self):
+        return self._path
 
     def load(self, another_path):
+
+        print("Loading: " + str(another_path))
 
         child_file_path = os.path.abspath(self.relativePath(another_path))
         child_dir = os.path.dirname(child_file_path)
         child_name = os.path.splitext(os.path.basename(child_file_path))[0]
 
-        old_path = self.path
+        old_path = self._path
         old_sys_path = sys.path;
 
-        self.path = Path(child_dir)
+        self._path = Path(child_dir)
         if sys.path[0] != child_dir:
             sys.path.insert(0,child_dir)
 
         runpy.run_module(child_name,  { "pipeline": self }, run_name = "")
 
-        self.path = old_path;
+        self._path = old_path;
         sys.path = old_sys_path
 
     def save(self, path=None):
@@ -47,7 +51,7 @@ class Pipeline:
             path = self.defaultSavePath()
 
         f = open(path, 'w')
-        f.write(json.dumps(self.tasks, indent=4))
+        f.write(json.dumps(self._tasks, indent=4))
         f.write('\n')
 
     def defaultSavePath(self):
--- python/pipeline/__main__.py
+++ python/pipeline/__main__.py
@@ -1,17 +1,18 @@
 import sys
 from . import Pipeline
+from pathlib import Path
 
 sys.dont_write_bytecode = True
 
 if len(sys.argv) > 1:
-    root_path = sys.argv[1]
-    savePath = None
+    root_file = Path(sys.argv[1])
+    save_file = None
     if len(sys.argv) > 2:
-        savePath = sys.argv[2]
+        save_file = sys.argv[2]
 
-    p = Pipeline(args = sys.argv[3:])
-    p.load(root_path)
-    p.save(savePath)
+    p = Pipeline(root_file.parent, args = sys.argv[3:])
+    p.load(root_file.name)
+    p.save(save_file)
 
 else:
     print("Required arguments: <generator script>")
